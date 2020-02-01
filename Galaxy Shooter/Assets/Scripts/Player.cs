642a77871e1079d61eb01b225b4ae8005ba48580
Krzysztof <cristopherb95@yahoo.com>
2020-02-01 20:42:59 +0100

    Project setup, added player prototype and movement support

---
+++ Galaxy Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public float _speed = 9.0f;
+
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        var direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+        //if (transform.position.y >= 0)
+        //{
+        //    transform.position = new Vector3(transform.position.x, 0, 0);
+        //}
+        //else if (transform.position.y <= -3.8f)
+        //{
+        //    transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        //}
+    }
+}
